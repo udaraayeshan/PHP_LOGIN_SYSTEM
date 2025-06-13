10a353df465f3cb1922fff3af0c4b72f2ebd13c7
udaraayeshan <ayeshan98udara@gmail.com>
2025-06-13 15:03:16 +0530

    Create login page and database connection

---
+++ loginAction.php
@@ -0,0 +1,29 @@
+<?php
+
+$u=$_POST["un"];
+$p=$_POST["pass"];
+
+$con=mysqli_connect("localhost","root","","logins");
+
+if(!$con){
+    echo "Database Donnection Failed";
+}
+
+
+$res=mysqli_query($con,"select * from user where UserN='$u' and Password='$p' ");
+
+    $b=false;              //boolean gannawa if ekata condition ekak ganna
+
+    while($row=mysqli_fetch_array($res)){
+    $b=true;
+}
+
+
+if($b){
+    echo "Login Success";
+}else{
+    echo "Login Falied";
+}
+ 
+
+?>
